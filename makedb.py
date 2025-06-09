43fc2d31cb48db2f227f3482dd2efc280618b3e9
swwon-i <stonesw123@naver.com>
2025-06-09 15:22:48 +0900

    hello

---
+++ makedb.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from pinecone import Pinecone, ServerlessSpec
+
+load_dotenv()
+
+pc = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
+
+pc.create_index(
+    name = os.environ['PINECONE_INDEX_NAME'], 
+    dimension = 1536,
+    metric = 'cosine',
+    spec = ServerlessSpec(
+        region = 'us-east-1',
+        cloud = 'aws'
+    )
+)
+
+wine_index = pc.Index(os.environ['PINECONE_INDEX_NAME'])
+
+print(wine_index.describe_index_stats())
